8c2b344e4ae1aadb88433fd6a431ae4b7e0cb9db
fishguy6564 <fishguy6564@gmail.com>
2026-03-07 15:32:31 -0500

    Added headers for ECourseID

---
+++ include/mush/Course/ECourseID.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <cstdint>
+
+namespace mush
+{
+    class ECourseID
+    {
+        public:
+            enum ECourseID_ : int32_t {};
+
+            ECourseID_ mValue;
+            
+            const char* text_(int);
+
+            ECourseID(ECourseID_ item) : mValue(item) {}
+            ECourseID(int32_t item) : mValue(static_cast<ECourseID_>(item)) {}
+    };
+}
