ef514d0cf8b2dcb9b77420cad0620a02a816d160
fishguy6564 <fishguy6564@gmail.com>
2026-06-28 23:23:26 -0400

    Page_CourseBattle

--- include/ui/Page/Page_CourseBattle.hpp
+++ include/ui/Page/Page_CourseBattle.hpp
@@ -5,6 +5,8 @@
 #include "gear/UI/Page/UIPage.hpp"
 #include "gear/UI/Flow/UIFlow_Open.hpp"
 
+#include "ui/Buttons/Control_CourseButton.hpp"
+
 namespace ui
 {
     class Page_CourseBattle : public gear::UIPage
@@ -13,5 +15,8 @@ namespace ui
         gear::UIFlow_Open mFlowOpen1; // 0x1D0
         gear::UIFlow mFlow1; // 0x320
         gear::UIFlow_Open mFlowOpen2; // 0x348
+        uint8_t mPad498[0x30]; // 0x498
+        gear::UIControl* mLayoutParent; // 0x4C8
+        Control_CourseButton* mCourseButtons[8]; // 0x4D0
     };
 }
